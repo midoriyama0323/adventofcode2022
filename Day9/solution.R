fc6bdb03a9a32af918797b7cd7fd9d4c74f974a9
Richard Illyes <richard.illyes@givaudan.com>
2022-12-09 16:33:43 +0100

    Day9 create dict and list

--- Day9/solution.R
+++ Day9/solution.R
@@ -1,2 +1,8 @@
 library("tidyverse")
-input <- readLines("Day9\\input.txt")
+input <- read.table("Day9\\input.txt")
+
+head_loc <- c(0,0)
+tail_loc <- c(0,0)
+move <- c("U"=c(0,1), "D"=c(0,-1), "R"=c(1,0), "L"=c(-1,0))
+
+tail_loc_list <- list(tail_loc)
